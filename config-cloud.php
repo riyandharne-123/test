345765ea961865e2c88a395626767d293f0c9f46
riyandharne-123 <60026968+riyandharne-123@users.noreply.github.com>
2020-03-16 12:52:33 +0530

    Add files via upload

---
+++ config-cloud.php
@@ -0,0 +1,8 @@
+<?php
+\Cloudinary::config(array( 
+  "cloud_name" => "dzn7xbwff", 
+  "api_key" => "229192936641588", 
+  "api_secret" => "DmdDdUUAWsLsEeoKtu-KpmGtZZg", 
+  "secure" => true
+));
+?>
